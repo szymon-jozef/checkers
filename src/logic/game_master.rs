b031d4894a60ab128193a2412238004560fcc93d
szymon <szymon_jozef@proton.me>
2026-08-14 14:03:52 +0200

    feat: add checking if player has lost

--- src/logic/game_master.rs
+++ src/logic/game_master.rs
@@ -3,9 +3,13 @@ use crate::logic::{
         board::Board,
         pawn::{CapturePath, MovePath},
     },
-    math::position::Position,
     player::Player,
 };
+
+pub enum GameResult {
+    Lost(uuid::Uuid),
+    Draw,
+}
 
 struct PlayerList {
     pub players: [Player; 2],
@@ -28,6 +32,10 @@ impl PlayerList {
 
     pub fn get_current_turn(&self) -> &Player {
         &self.players[self.current_turn]
+    }
+
+    pub fn get_passive_player(&self) -> &Player {
+        &self.players[(self.current_turn + 1) % self.size]
     }
 }
 
@@ -68,5 +76,32 @@ impl GameMaster {
             })
             .flatten()
             .collect()
+    }
+
+    fn is_player_lost(&self, player: &Player) -> bool {
+        let player_pawns = self.board.get_player_pawns_positions(player);
+
+        player_pawns.len() == 0
+            || player_pawns
+                .iter()
+                .all(|pos| self.board.get_available_moves(*pos, player).is_none())
+    }
+
+    pub fn get_game_result(&self) -> Option<GameResult> {
+        let active: &Player = self.players.get_current_turn();
+        let passive: &Player = self.players.get_passive_player();
+
+        let active_lost: bool = self.is_player_lost(active);
+        let passive_lost: bool = self.is_player_lost(passive);
+
+        if active_lost && passive_lost {
+            Some(GameResult::Draw)
+        } else if active_lost {
+            Some(GameResult::Lost(active.id))
+        } else if passive_lost {
+            Some(GameResult::Lost(passive.id))
+        } else {
+            None
+        }
     }
 }
